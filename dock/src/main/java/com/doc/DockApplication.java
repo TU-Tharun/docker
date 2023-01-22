bb10d72f370fea844edfe3857dae2fd15de308db
TU-Tharun <123296854+TU-Tharun@users.noreply.github.com>
2023-01-22 15:18:02 +0530

    Add files via upload

---
+++ dock/src/main/java/com/doc/DockApplication.java
@@ -0,0 +1,21 @@
+package com.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class DockApplication {
+	@GetMapping("/demo")
+	public String demo() {
+		return "hello world";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockApplication.class, args);
+	}
+
+}
